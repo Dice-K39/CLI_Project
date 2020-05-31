7a5eeb5d14e5cd87105061a6bec3704e287cef54
Dice-K39 <daisukehashimoto39@gmail.com>
2020-05-31 02:11:08 -0400

    WIP: Saving work but API not working.

--- lib/game_cli/api.rb
+++ lib/game_cli/api.rb
@@ -1,3 +1,15 @@
 class API
-    puts "API active"
+    BASE_URL = "https://rawg-video-games-database.p.rapidapi.com/games"
+    KEY = ENV["API_KEY"]
+
+    def self.get_games
+        response = RestClient.get("#{BASE_URL}#{KEY}")
+        data = JSON.parse(response.body)
+
+        data["results"].each do |game|
+            puts game["name"]
+        end
+        
+        binding.pry
+    end
 end
