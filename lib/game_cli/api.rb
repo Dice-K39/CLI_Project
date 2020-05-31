60b526edb3c085a1b16e10d4d5db6ee6e306015d
Dice-K39 <daisukehashimoto39@gmail.com>
2020-05-31 18:44:37 -0400

    WIP: Coded API class to get data successfully.

--- lib/game_cli/api.rb
+++ lib/game_cli/api.rb
@@ -1,15 +1,21 @@
 class API
-    BASE_URL = "https://rawg-video-games-database.p.rapidapi.com/games"
-    KEY = ENV["API_KEY"]
+    URL = URI("https://rawg-video-games-database.p.rapidapi.com/games?search=\"Doom Eternal\"")
 
     def self.get_games
-        response = RestClient.get("#{BASE_URL}#{KEY}")
-        data = JSON.parse(response.body)
+        search_key = gets.chomp
+        http = Net::HTTP.new(URL.host, URL.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-        data["results"].each do |game|
-            puts game["name"]
-        end
+        request = Net::HTTP::Get.new(URL)
+        request["x-rapidapi-host"] = 'rawg-video-games-database.p.rapidapi.com'
+        request["x-rapidapi-key"] = ENV["API_KEY"]
         
+        response = http.request(request)
+        data = JSON.parse(response.body)
         binding.pry
+        data["results"].each.with_index(1) do |game, i|
+            puts "#{i}. #{game["name"]}"
+        end
     end
 end
